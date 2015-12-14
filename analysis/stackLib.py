8dcb7fb25eb159c91267a8a82ee3cd73aed9d8f0
Thomas Maillart <thomas.maillart@gmail.com>
2015-12-13 19:54:18 -0800

    first look at data

--- analysis/stackLib.py
+++ analysis/stackLib.py
@@ -1,7 +1,9 @@
+import numpy as np
+import pylab as pl
+from scipy import stats as S
+
 import boto
-
-import json
-
+import zlib,json
 import pandas as pd
 
 def connectBucket(bucketName):
@@ -9,34 +11,131 @@ def connectBucket(bucketName):
     bucket = s3.get_bucket(bucketName)
     return bucket
 
-def getSites():
-    key = bucket.get_key("main/sites.json")
-    sites = json.loads(key.get_contents_as_string())
-    sites.pop("stackoverflow",None)
-    return sites
+global bucketName
+bucketName = "stackperf"
 
-def retrieveDF(site):
-
-    key = bucket.get_key("DataFrames/%s"%site)
-
-#def parseQuestion
+global bucket
+bucket = connectBucket(bucketName)
 
 
+def rankorder(x):
+	x1 = list(np.sort(x))
+	x1.reverse()
+	y1 = range(1,len(x1)+1)
+	return np.array(x1),np.array(y1)
 
-if __name__ == '__main__':
 
-    global bucketName
-    bucketName = "stackoverflow_data"
-    
-    global bucket
-    bucket = connectBucket(bucketName)
+def logify(x,y):
+	from numpy import log10,zeros_like
 
-    global sites
-    sites = getSites()
+	x = np.array(x)
+	y = np.array(y)
 
-    
-    
-#J = json.loads(zlib.decompress(bytes(bytearray(open("tmp/archive.gz",'rb').read())),15 + 16))
-#J = json.loads(zlib.decompress(bytes(bytearray(key.get_contents_as_string())),15 + 16))
-        
-    
+	c = (x>0)*(y>0)
+
+	lx = log10(x[c])
+	ly = log10(y[c])
+
+	return lx,ly
+
+def binning(x,y,bins,log_10=False,confinter=5):
+    '''makes a simple binning'''
+
+    x = np.array(x);y = np.array(y)
+
+    if isinstance(bins,int) or isinstance(bins,float):
+        bins = np.linspace(np.min(x)*0.9,np.max(x)*1.1,bins)
+    else:
+        bins = np.array(bins)
+
+    if log_10:
+        bins = bins[bins>0]
+        c = x > 0
+        x = x[c]
+        y = y[c]
+        bins = np.log10(bins)
+        x = np.log10(x)
+        y = np.log10(y)
+
+    Tbins = []
+    Median = []
+    Mean = []
+    Sigma =[]
+    Perc_Up = []
+    Perc_Down = []
+    Points=[]
+
+
+    for i,ix in enumerate(bins):
+        if i+2>len(bins):
+            break
+
+        c1 = x >= ix
+        c2 = x < bins[i+1]
+        c=c1*c2
+
+        if len(y[c])>0:
+            Tbins = np.append(Tbins,np.median(x[c]))
+            Median =  np.append(Median,np.median(y[c]))
+            Mean = np.append(Mean,np.mean(y[c]))
+            Sigma = np.append(Sigma,np.std(y[c]))
+            Perc_Down = np.append(Perc_Down,np.percentile(y[c],confinter))
+            Perc_Up = np.append(Perc_Up,np.percentile(y[c],100 - confinter))
+            Points = np.append(Points,len(y[c]))
+
+
+    return {'bins' : Tbins,
+            'median' : Median,
+            'mean' : Mean,
+            'stdDev' : Sigma,
+            'percDown' :Perc_Down,
+            'percUp' :Perc_Up,
+            'nPoints' : Points}
+
+
+def answerCDF(api_site_name):
+
+    try:
+        key = bucket.get_key("QA/%s.json.zlib"%api_site_name)
+        items = json.loads(zlib.decompress(key.get_contents_as_string()))
+    except:
+        "print data not found on S3. Please crawl data from stackexchange website first"
+        return
+
+    A = []
+    S = []
+    Sa = []
+    rank = []
+    score = []
+    time = []
+
+    for item in items:
+        a = 0
+        s = 0
+        t0 = item['creation_date']
+
+
+        if item.has_key('answers'):
+            for i,answer in enumerate(item['answers']):
+
+                a += 1
+                dt = answer['creation_date'] - t0
+                time.append(dt)
+
+                score.append(answer['score'])
+                s += answer['score']
+
+                rank.append(i+1)
+
+                #t0 = answer['creation_date']
+            A.append(a)
+            Sa.append(s)
+            S.append(item['score'])
+
+    return {'rank' : np.array(rank),
+            'score' : np.array(score),
+            'time' : np.array(time),
+            'A': np.array(A),
+            'S': np.array(S),
+            'Sa': np.array(Sa),
+            }
